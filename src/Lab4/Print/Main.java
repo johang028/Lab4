b994afbf6aa79041039fd3061293c1bfe8dbdd93
johang0128 <jgaleza28@gmail.com>
2021-11-08 20:36:06 -0500

    Initial commit

---
+++ src/Lab4/Print/Main.java
@@ -0,0 +1,16 @@
+/*Johan Galeza
+jgaleza1@umbc.edu*/
+
+package Lab4.Print;
+public class Main {
+
+    public static void main(String[] args) {
+    int x, y;
+    for(x=10;x>=1;x--) {
+    for (y = 1; y <= x; y++) {
+    System.out.print("*");
+        }
+    System.out.println();
+        }
+    }
+}
